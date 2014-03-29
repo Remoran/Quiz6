2cab94e1f21705ad54c01321bbbda134ead203dc
Remoran <ryan@mmoran.com>
2014-03-28 22:05:55 -0400

    Initial Commit

---
+++ Quiz6/Quiz6AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Quiz6AppDelegate.h
+//  Quiz6
+//
+//  Created by Ryan on 3/28/14.
+//  Copyright (c) 2014 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz6AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz6/Quiz6MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  Quiz6MasterViewController.h
+//  Quiz6
+//
+//  Created by Ryan on 3/28/14.
+//  Copyright (c) 2014 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz6MasterViewController : UITableViewController
+
+@end
